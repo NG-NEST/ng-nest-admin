130bba93279213b3663c1bc9d10899cc5526ec7d
ng-moon <ngmoon2019@163.com>
2019-09-23 22:40:22 +0800

    add: project

---
+++ ui/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, Inject } from "@angular/core";
+import { DOCUMENT } from "@angular/common";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+})
+export class AppComponent {
+  title = "ng-nest-admin-ui";
+  constructor(@Inject(DOCUMENT) private doc: Document) {
+    let color = getComputedStyle(this.doc.documentElement).getPropertyValue(
+      "--nm-primary-color"
+    );
+    console.log(color);
+  }
+}
