fe770e87a212ea416655141a022fb8c5878025f7
ng-nest-moon <ngnest@163.com>
2019-09-26 23:31:25 +0800

    add: dashboard

--- ui/src/main/login/login.module.ts
+++ ui/src/main/login/login.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login.component';
-import { LoginRoutesModule } from './login-routes.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ShareModule } from 'src/share/share.module';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LoginComponent } from "./login.component";
+import { LoginRoutesModule } from "./login-routes.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ShareModule } from "src/share/share.module";
 
 @NgModule({
   imports: [
     CommonModule,
-    LoginRoutesModule,
-    ReactiveFormsModule,
     FormsModule,
-    ShareModule
+    ReactiveFormsModule,
+    ShareModule,
+    LoginRoutesModule
   ],
   declarations: [LoginComponent]
 })
-export class LoginModule { }
+export class LoginModule {}
