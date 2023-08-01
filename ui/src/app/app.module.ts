e34d8823fd3f125602fa739ce032c49c13e14174
ng-nest-moon <ngnest@163.com>
2023-08-01 22:31:56 +0800

    feat: add aside menu

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppAuthService, AppInitializer } from '@ui/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, AppRoutingModule],
   providers: [
     {
       provide: APP_INITIALIZER,
--- ui/src/app/layout/index/index.module.ts
+++ ui/src/app/layout/index/index.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index.component';
-import { XContainerModule } from '@ng-nest/ui/container';
 import { IndexRoutingModule } from './index-routing.module';
 import { AsideComponent } from './aside/aside.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { CrumbComponent } from './crumb/crumb.component';
 import { ContentComponent } from './content/content.component';
+import { XMenuModule } from '@ng-nest/ui/menu';
 
 @NgModule({
   declarations: [IndexComponent, AsideComponent, TabsComponent, CrumbComponent, ContentComponent],
-  imports: [CommonModule, IndexRoutingModule, XContainerModule]
+  imports: [CommonModule, IndexRoutingModule, XMenuModule]
 })
 export class IndexModule {}
